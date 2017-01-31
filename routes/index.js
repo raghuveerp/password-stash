fcbc05e8b0998d8d7bac866bf926a8ff7aef8abe
Raghuveer Phull <rphull@apple.com>
2017-01-30 16:15:28 -0800

    creating first version of API docs

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,22 @@ var express    = require('express'),
     _          = require('lodash'),
     router     = express.Router();
 
+/**
+ * @apiDefine CreateUserError
+ * @apiVersion 0.1.0
+ *
+ * @apiError NoAccessRight Only authenticated Admins can access the data.
+ * @apiError UserNameTooShort Minimum of 5 characters required.
+ *
+ * @apiErrorExample  Response (example):
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": "NoAccessRight"
+ *     }
+ */
+
+
+
 var checkAllProperties = function (input) {
     var properties      = ['username', 'password', 'name', 'description', 'active'],
         inputProperties = [];
